7d58d602e6d7bfcdbb0856db73224ea1b9922444
ruby -v <ruby -v>
2019-03-14 09:44:10 +0600

    olesh

---
+++ tests/testcases/case8.js
@@ -0,0 +1,12 @@
+module.exports = {
+'tags' : ['case8'],
+'test artsyjewels' : function (browser) {
+browser
+.url('https://kaspi.kz/') // Go to a url
+.waitForElementVisible('body', 1000) // wait till page loads
+.click('#headerRegionConfirm__ConfirmButton')
+
+.pause(1000)
+.end();
+}
+};
